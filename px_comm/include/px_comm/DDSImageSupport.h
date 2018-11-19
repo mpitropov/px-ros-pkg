a9c1beff73990c760143192ae2d2bc2d37592e02
Matthew Pitropov <matthewpitropov@gmail.com>
2018-11-19 13:33:23 -0500

    Updated to RTI 5.3.1

--- px_comm/include/px_comm/DDSImageSupport.h
+++ px_comm/include/px_comm/DDSImageSupport.h
@@ -1,11 +1,11 @@
 
 /*
-  WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
 
-  This file was generated from DDSImage.idl using "rtiddsgen".
-  The rtiddsgen tool is part of the RTI Connext distribution.
-  For more information, type 'rtiddsgen -help' at a command shell
-  or consult the RTI Connext manual.
+This file was generated from DDSImage.idl using "rtiddsgen".
+The rtiddsgen tool is part of the RTI Connext distribution.
+For more information, type 'rtiddsgen -help' at a command shell
+or consult the RTI Connext manual.
 */
 
 #ifndef DDSImageSupport_248003145_h
@@ -14,58 +14,49 @@
 /* Uses */
 #include "DDSImage.h"
 
-#ifdef __cplusplus
 #ifndef ndds_cpp_h
-  #include "ndds/ndds_cpp.h"
-#endif
-#else
-#ifndef ndds_c_h
-  #include "ndds/ndds_c.h"
-#endif
+#include "ndds/ndds_cpp.h"
 #endif
 
-        
-namespace px_comm {
-    /* ========================================================================= */
-    /**
-       Uses:     T
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
 
-       Defines:  TTypeSupport, TDataWriter, TDataReader
+class __declspec(dllimport) DDSTypeSupport;
+class __declspec(dllimport) DDSDataWriter;
+class __declspec(dllimport) DDSDataReader;
 
-       Organized using the well-documented "Generics Pattern" for
-       implementing generics in C and C++.
-    */
+#endif
 
-    #if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
-      /* If the code is building on Windows, start exporting symbols.
-       */
-    #undef NDDSUSERDllExport
-      #define NDDSUSERDllExport __declspec(dllexport)
+/* ========================================================================= */
+/**
+Uses:     T
 
-    #endif
+Defines:  TTypeSupport, TDataWriter, TDataReader
 
-    #ifdef __cplusplus
+Organized using the well-documented "Generics Pattern" for
+implementing generics in C and C++.
+*/
 
-    DDS_TYPESUPPORT_CPP(DDSImageTypeSupport, DDSImage);
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, start exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport __declspec(dllexport)
 
-    DDS_DATAWRITER_CPP(DDSImageDataWriter, DDSImage);
-    DDS_DATAREADER_CPP(DDSImageDataReader, DDSImageSeq, DDSImage);
+#endif
 
-    #else
+DDS_TYPESUPPORT_CPP(
+    px_comm_DDSImageTypeSupport, 
+    px_comm_DDSImage);
 
-    DDS_TYPESUPPORT_C(DDSImageTypeSupport, DDSImage);
-    DDS_DATAWRITER_C(DDSImageDataWriter, DDSImage);
-    DDS_DATAREADER_C(DDSImageDataReader, DDSImageSeq, DDSImage);
+DDS_DATAWRITER_CPP(px_comm_DDSImageDataWriter, px_comm_DDSImage);
+DDS_DATAREADER_CPP(px_comm_DDSImageDataReader, px_comm_DDSImageSeq, px_comm_DDSImage);
 
-    #endif
-
-    #if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
-      /* If the code is building on Windows, stop exporting symbols.
-       */
-    #undef NDDSUSERDllExport
-      #define NDDSUSERDllExport
-    #endif
-} /* namespace px_comm  */
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+/* If the code is building on Windows, stop exporting symbols.
+*/
+#undef NDDSUSERDllExport
+#define NDDSUSERDllExport
+#endif
 
 #endif  /* DDSImageSupport_248003145_h */
 
